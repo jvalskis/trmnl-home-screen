d8041f192a8643bb4656ab177c0feded597cccde
Justas Valskis <4158759+jvalskis@users.noreply.github.com>
2026-02-24 00:46:29 +0200

    Persist only entities that are configured

--- src/main/scala/is/valsk/trmnlhomescreen/hass/protocol/handlers/GetStatesHandler.scala
+++ src/main/scala/is/valsk/trmnlhomescreen/hass/protocol/handlers/GetStatesHandler.scala
@@ -1,6 +1,6 @@
 package is.valsk.trmnlhomescreen.hass.protocol.handlers
 
-import is.valsk.trmnlhomescreen.hass.EntityState
+import is.valsk.trmnlhomescreen.hass.{EntityState, HomeAssistantConfig}
 import is.valsk.trmnlhomescreen.hass.messages.responses.*
 import is.valsk.trmnlhomescreen.hass.messages.{HassIdentifiableMessage, HassResponseMessage, Type}
 import is.valsk.trmnlhomescreen.hass.protocol.api.{EntityStateRepository, RequestRepository}
@@ -12,16 +12,20 @@ import zio.json.*
 class GetStatesHandler(
     entityStateRepository: EntityStateRepository,
     val requestRepository: RequestRepository,
+    allowedEntityIds: Set[String],
 ) extends HomeAssistantResultHandler {
 
   override def handleInternal(channel: WebSocketChannel, result: HassResponseMessage & HassIdentifiableMessage) =
     result match {
       case result: Result =>
         ZIO.foreachDiscard(
-          result.result.toSeq.flatMap(_.toJson.fromJson[List[EntityState]].toOption).flatten,
+          result.result.toSeq
+            .flatMap(_.toJson.fromJson[List[EntityState]].toOption)
+            .flatten
+            .filter(state => allowedEntityIds.contains(state.entityId)),
         )(state =>
           entityStateRepository.add(state.entityId, state) *>
-            ZIO.logDebug(s"Updated entity state for ${state.entityId}"),
+            ZIO.logDebug(s"Updated entity state for ${state.entityId} -> $state"),
         )
       case _ => ZIO.unit
     }
@@ -31,11 +35,12 @@ class GetStatesHandler(
 
 object GetStatesHandler {
 
-  val layer: URLayer[RequestRepository & EntityStateRepository, GetStatesHandler] = ZLayer {
+  val layer: URLayer[RequestRepository & EntityStateRepository & HomeAssistantConfig, GetStatesHandler] = ZLayer {
     for {
       requestRepository <- ZIO.service[RequestRepository]
       entityStateRepository <- ZIO.service[EntityStateRepository]
-    } yield GetStatesHandler(entityStateRepository, requestRepository)
+      config <- ZIO.service[HomeAssistantConfig]
+    } yield GetStatesHandler(entityStateRepository, requestRepository, config.subscribedEntityIdList.toSet)
   }
 
 }
