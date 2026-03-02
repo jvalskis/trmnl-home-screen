8dedfd7234e919a7880895a220f49f4dfd815511
Justas Valskis <4158759+jvalskis@users.noreply.github.com>
2026-03-03 01:27:50 +0200

    Properties extractor model

--- src/main/scala/is/valsk/trmnlhomescreen/calendar/CalendarPropertiesExtractor.scala
+++ src/main/scala/is/valsk/trmnlhomescreen/calendar/CalendarPropertiesExtractor.scala
@@ -1,6 +1,7 @@
 package is.valsk.trmnlhomescreen.calendar
 
 import is.valsk.trmnlhomescreen.PropertiesExtractor
+import is.valsk.trmnlhomescreen.PropertiesExtractor.{MapProperty, PropertyEntry, asScalar}
 import zio.{RLayer, UIO, URLayer, ZIO, ZLayer}
 
 import java.time.{LocalDate, ZoneId}
@@ -16,40 +17,46 @@ object CalendarPropertiesExtractor:
       repository <- ZIO.service[CalendarStateRepository]
       config <- ZIO.service[CalendarConfig]
     } yield new PropertiesExtractor:
-      def extract: UIO[Map[String, Any]] = {
+      def extract: UIO[MapProperty] = {
         val currentDate = LocalDate.now(ZoneId.of("Europe/Vilnius"))
         repository.get.map { events =>
-          Seq(
-            "calendar_enabled" -> config.enabled,
-            "events" -> events.sortBy(_.startDate).map(createEventInfo(_).asJava).asJava,
-            "events_by_day" -> (0 to config.daysAhead).map { daysSinceToday =>
-              val date = currentDate.plusDays(daysSinceToday)
-              Map(
-                "days_since_today" -> daysSinceToday,
-                "events" -> events.filter(_.happensOnDay(date)).map(createEventInfo(_).asJava).asJava,
-                "date" -> date,
-                "day_of_week" -> date.getDayOfWeek,
-              ).asJava
-            }.asJava,
-          ).toMap
+          MapProperty(
+            "calendar_enabled" -> config.enabled.asScalar,
+            "events" -> events.sortBy(_.startDate).map(createEventInfo).asList,
+            "events_by_day" -> (0 to config.daysAhead)
+              .map { daysSinceToday =>
+                val date = currentDate.plusDays(daysSinceToday)
+                MapProperty(
+                  "days_since_today" -> daysSinceToday.asScalar,
+                  "events" -> events.filter(_.happensOnDay(date)).map(createEventInfo).asList,
+                  "date" -> date.asScalar,
+                  "day_of_week" -> date.getDayOfWeek.asScalar,
+                )
+              }
+              .asList,
+          )
         }
 
       }
   }
 
   extension (event: CalendarEvent) {
+
     private def happensOnDay(day: LocalDate) = {
-      (event.startDate.toLocalDate.isEqual(day) || event.startDate.toLocalDate.isBefore(day)) && (event.endDate.exists(_.toLocalDate.isEqual(day)) || event.endDate.exists(_.toLocalDate.isAfter(day)))
+      (event.startDate.toLocalDate.isEqual(day) || event.startDate.toLocalDate.isBefore(day)) && (event.endDate.exists(
+        _.toLocalDate.isEqual(day),
+      ) || event.endDate.exists(_.toLocalDate.isAfter(day)))
     }
+
   }
 
-  private def createEventInfo(event: CalendarEvent): Map[String, Any] =
-    Map(
-      "summary" -> event.summary,
-      "start" -> event.startDate.format(displayFormatter),
-      "end" -> event.endDate.fold("")(_.format(displayFormatter)),
-      "location" -> event.location.getOrElse(""),
-      "description" -> event.description.getOrElse(""),
+  private def createEventInfo(event: CalendarEvent): PropertyEntry =
+    MapProperty(
+      "summary" -> event.summary.asScalar,
+      "start" -> event.startDate.format(displayFormatter).asScalar,
+      "end" -> event.endDate.fold("")(_.format(displayFormatter)).asScalar,
+      "location" -> event.location.getOrElse("").asScalar,
+      "description" -> event.description.getOrElse("").asScalar,
     )
 
   val configuredLayer: RLayer[CalendarStateRepository, PropertiesExtractor] = CalendarConfig.layer >>> layer
--- src/main/scala/is/valsk/trmnlhomescreen/weather/WeatherPropertiesExtractor.scala
+++ src/main/scala/is/valsk/trmnlhomescreen/weather/WeatherPropertiesExtractor.scala
@@ -1,6 +1,7 @@
 package is.valsk.trmnlhomescreen.weather
 
 import is.valsk.trmnlhomescreen.PropertiesExtractor
+import is.valsk.trmnlhomescreen.PropertiesExtractor.{MapProperty, asScalar}
 import zio.{RLayer, UIO, URLayer, ZIO, ZLayer}
 
 object WeatherPropertiesExtractor:
@@ -10,53 +11,53 @@ object WeatherPropertiesExtractor:
       repository <- ZIO.service[WeatherStateRepository]
       config <- ZIO.service[WeatherConfig]
     } yield new PropertiesExtractor:
-      def extract: UIO[Map[String, Any]] =
+      def extract: UIO[MapProperty] =
         for {
           maybeCurrentConditions <- repository.get
-          result = maybeCurrentConditions.toSeq.flatMap { conditions =>
+          result = MapProperty(maybeCurrentConditions.toSeq.flatMap { conditions =>
             Seq(
-              Nil :+ "weather_text" -> conditions.weatherText,
-              Nil :+ "weather_icon" -> conditions.weatherIcon,
-              Nil :+ "temp_metric_value" -> conditions.temperature.metric.value,
-              Nil :+ "temp_metric_unit" -> conditions.temperature.metric.unit,
-              Nil :+ "temp_imperial_value" -> conditions.temperature.imperial.value,
-              Nil :+ "temp_imperial_unit" -> conditions.temperature.imperial.unit,
-              Nil :+ "has_precipitation" -> conditions.hasPrecipitation,
-              Nil :+ "is_day_time" -> conditions.isDayTime,
-              Nil :+ "observation_time" -> conditions.localObservationDateTime,
-              conditions.relativeHumidity.toSeq.map("relative_humidity" -> _),
-              conditions.cloudCover.toSeq.map("cloud_cover" -> _),
-              conditions.uvIndex.toSeq.map("uv_index" -> _),
-              conditions.uvIndexText.toSeq.map("uv_index_text" -> _),
+              Nil :+ "weather_text" -> conditions.weatherText.asScalar,
+              Nil :+ "weather_icon" -> conditions.weatherIcon.asScalar,
+              Nil :+ "temp_metric_value" -> conditions.temperature.metric.value.asScalar,
+              Nil :+ "temp_metric_unit" -> conditions.temperature.metric.unit.asScalar,
+              Nil :+ "temp_imperial_value" -> conditions.temperature.imperial.value.asScalar,
+              Nil :+ "temp_imperial_unit" -> conditions.temperature.imperial.unit.asScalar,
+              Nil :+ "has_precipitation" -> conditions.hasPrecipitation.asScalar,
+              Nil :+ "is_day_time" -> conditions.isDayTime.asScalar,
+              Nil :+ "observation_time" -> conditions.localObservationDateTime.asScalar,
+              conditions.relativeHumidity.toSeq.map("relative_humidity" -> _.asScalar),
+              conditions.cloudCover.toSeq.map("cloud_cover" -> _.asScalar),
+              conditions.uvIndex.toSeq.map("uv_index" -> _.asScalar),
+              conditions.uvIndexText.toSeq.map("uv_index_text" -> _.asScalar),
               conditions.wind.toSeq.flatMap { wind =>
                 Seq(
-                  "wind_speed_metric_value" -> wind.speed.metric.value,
-                  "wind_speed_metric_unit" -> wind.speed.metric.unit,
-                  "wind_speed_imperial_value" -> wind.speed.imperial.value,
-                  "wind_speed_imperial_unit" -> wind.speed.imperial.unit,
-                  "wind_direction" -> wind.direction.localized,
-                  "wind_direction_degrees" -> wind.direction.degrees,
+                  "wind_speed_metric_value" -> wind.speed.metric.value.asScalar,
+                  "wind_speed_metric_unit" -> wind.speed.metric.unit.asScalar,
+                  "wind_speed_imperial_value" -> wind.speed.imperial.value.asScalar,
+                  "wind_speed_imperial_unit" -> wind.speed.imperial.unit.asScalar,
+                  "wind_direction" -> wind.direction.localized.asScalar,
+                  "wind_direction_degrees" -> wind.direction.degrees.asScalar,
                 )
               },
               conditions.visibility.toSeq.flatMap { vis =>
                 Seq(
-                  "visibility_metric_value" -> vis.metric.value,
-                  "visibility_metric_unit" -> vis.metric.unit,
-                  "visibility_imperial_value" -> vis.imperial.value,
-                  "visibility_imperial_unit" -> vis.imperial.unit,
+                  "visibility_metric_value" -> vis.metric.value.asScalar,
+                  "visibility_metric_unit" -> vis.metric.unit.asScalar,
+                  "visibility_imperial_value" -> vis.imperial.value.asScalar,
+                  "visibility_imperial_unit" -> vis.imperial.unit.asScalar,
                 )
               },
               conditions.realFeelTemperature.toSeq.flatMap { realFeel =>
                 Seq(
-                  "real_feel_metric_value" -> realFeel.metric.value,
-                  "real_feel_metric_unit" -> realFeel.metric.unit,
-                  "real_feel_imperial_value" -> realFeel.imperial.value,
-                  "real_feel_imperial_unit" -> realFeel.imperial.unit,
+                  "real_feel_metric_value" -> realFeel.metric.value.asScalar,
+                  "real_feel_metric_unit" -> realFeel.metric.unit.asScalar,
+                  "real_feel_imperial_value" -> realFeel.imperial.value.asScalar,
+                  "real_feel_imperial_unit" -> realFeel.imperial.unit.asScalar,
                 )
               },
             )
-          }.flatten.toMap
-        } yield result + ("weather_enabled" -> config.enabled)
+          }.flatten: _*)
+        } yield result + ("weather_enabled" -> config.enabled.asScalar)
   }
 
   val configuredLayer: RLayer[WeatherStateRepository, PropertiesExtractor] = WeatherConfig.layer >>> layer
